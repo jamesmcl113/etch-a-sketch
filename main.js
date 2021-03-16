9210fa600f316f4fac6831d3bf66e15933eadf06
James Mclaughlin <sc19jm2@leeds.ac.uk>
2021-03-16 10:48:33 +0000

    final build

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const grid = document.querySelector("#grid")
 
-const SIZE = 16
+let size = 16
 
 window.onload = function() {
-    grid.style.gridTemplateColumns = 'repeat(' + String(SIZE) + ', 1fr)'
+    grid.style.gridTemplateColumns = 'repeat(' + String(size) + ', 1fr)'
 
-    for(let i = 0; i < SIZE*SIZE; i++)
+    for(let i = 0; i < size*size; i++)
     {
         let d = document.createElement("div")
         d.classList = "grid-block"
         d.style = "background-color: white;"
-        d.addEventListener("mouseover", changeColour);
+        d.addEventListener("mouseover", changeColour)
         grid.appendChild(d)
     }
 }
@@ -25,5 +25,32 @@ function changeColour(e)
 
 function changeSize()
 {
-    let newSize = prompt("Enter")
+    let newSize = prompt("Enter new size (10 - 100)", "20")
+    newSize = parseInt(newSize)
+
+    if(!isNaN(newSize))
+    {
+        if(newSize >= 10 && newSize <= 100) {
+            grid.innerHTML = '';
+            
+            grid.style.gridTemplateColumns = 'repeat(' + String(newSize) + ', 1fr)'
+            for(let i = 0; i < newSize*newSize; i++)
+            {
+                let d = document.createElement("div")
+                d.classList = "grid-block"
+                d.style = "background-color: white;"
+                d.addEventListener("mouseover", changeColour)
+                grid.appendChild(d)
+            }
+            size = newSize
+        }
+    }
+}
+
+function clearGrid()
+{
+    for(e of grid.children)
+    {
+        e.style.background = "white"
+    }
 }
